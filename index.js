48dc41110d7ffb8f5f0f282bbd24e8aa0606bc94
Derek Severson <derek@swarmingtech.com>
2016-11-28 12:49:52 -0600

    Implement

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 'use strict';
 
-exports = module.exports = function () {
+var isString = require('lodash.isstring');
+var isFunction = require('lodash.isfunction');
 
+exports = module.exports = function (name, validationFn, errorMessage) {
 
+  if (!isString(name)) {
+    throw new Error('Invalid Argument');
+  }
+
+  var isValid = isFunction(validationFn) ? validationFn : exists;
+
+  if (!isValid(process.env[name])) {
+
+    if (!isString(errorMessage)) {
+      errorMessage = (isValid === exists)
+        ? 'Missing Environment Variable "' + name + '"'
+        : 'Invalid Environment Variable "' + name + '"';
+    }
+
+    throw new Error(errorMessage);
+  }
+
+  return process.env[name];
 };
+
+// ---
+
+function exists(val) {
+  return val != null;
+}
